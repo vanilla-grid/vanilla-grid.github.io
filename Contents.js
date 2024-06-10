26336872a2759f0b57ae123eaa78179bfdc76745
vanilla-grid <vanilla.grid.co@gmail.com>
2024-06-10 13:55:05 +0900

    Create Contents.js

---
+++ Contents.js
@@ -0,0 +1,43 @@
+function getComments() {
+    function Comments () {
+        this.common = {
+            kor: {
+                "1000" : "▼ 해당 속성은 아래와 같습니다.",
+                "1001" : "<div data-vanillanote></div>",
+                "1002" : "▼ 예시 코드는 아래와 같습니다.",
+                "1003" : "▼ 최 상단의 costomized된 editor는 해당 코드를 추가했습니다.",
+                "1004" : "변경 시 심각한 오류발생 가능성이 있음.",
+                "2000" : "문자열",
+                "2001" : "자연수",
+                "2002" : "부울",
+                "2003" : "정수",
+                "2004" : "배열",
+                "2005" : "객체",
+                "2006" : "요소",
+                "2007" : "함수",
+                "2008" : "양의 정수",
+                "3000" : "테스트 해보세요.",
+                "3001" : "해당 버튼을 클릭 후 콘솔창(F12)을 확인해 보세요.",
+            },
+            eng: {
+                "1000" : "The attributes are as follows.",
+                "1001" : "<div data-vanillanote></div>",
+                "1002" : "The example code is as follows.",
+                "1003" : "▼ Top customized editor has added this code",
+                "1004" : "There is a possibility of serious error when changing.",
+                "2000" : "String",
+                "2001" : "Natural number",
+                "2002" : "Boolean",
+                "2003" : "Integer",
+                "2004" : "Array",
+                "2005" : "Object",
+                "2006" : "Element",
+                "2007" : "Function",
+                "2008" : "positive integer",
+                "3000" : "Try it.",
+                "3001" : "Click the button beside and check the console window (F12).",
+            },
+        }
+    }
+    return new Comments();
+}
