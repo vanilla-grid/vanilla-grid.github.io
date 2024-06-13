ed250b606a8907199263c329a472d9a862610621
vanilla-grid <vanilla.grid.co@gmail.com>
2024-06-13 16:28:15 +0900

    Update SampleGrids.js

--- SampleGrids.js
+++ SampleGrids.js
@@ -9,7 +9,7 @@ function getSampleGrids(COMMENTS) {
             [keysIntro[0]] :
     `<vanilla-grid id="` + gridIdsIntro[0] + `">
         <v-col id="col1" header="header;column1;text" dataType="text" width="270px"></v-col>
-        <v-col id="col2" header="header;column2;code" dataType="code" codes="A;B;C;D;E" width="60px"></v-col>
+        <v-col id="col2" header="header;column2;code" dataType="code" codes="A;B;C;D;E" width="80px"></v-col>
         <v-col id="col3" header="header;column3;mask" dataType="mask" format="AAAA-999" width="120px"></v-col>
         <v-col id="col4" header="header;column4;date" dataType="date" footer="MAX;MIN;SUM;AVG" width="120px"></v-col>
         <v-col id="col5" header="header;column5;number" dataType="number" footer="$$MAX;$$MIN;$$SUM;$$AVG" format="$ #,##0.####" width="120px"></v-col>
