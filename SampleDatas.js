bca8c7b3cb8b5aca994b7d4151c05626b15a26b7
vanilla-grid <vanilla.grid.co@gmail.com>
2024-06-14 16:56:50 +0900

    Update SampleDatas.js

--- SampleDatas.js
+++ SampleDatas.js
@@ -6,26 +6,47 @@ function getSampleDatas(COMMENTS) {
             gridIdsIntro.push("grid" + keysIntro[i])
         }
 
-        const textKorIntro = COMMENTS.kor.intro.sampleText;
+        const textKorIntro = COMMENTS.kor.intro.grid;
         for(let r = 0; r < textKorIntro[keysIntro[0]].col1.length; r++) {
             textKorIntro[keysIntro[0]].col1[r].gridId = gridIdsIntro[0];
             textKorIntro[keysIntro[0]].col1[r].row = r + 1;
             textKorIntro[keysIntro[0]].col1[r].colId = "col1";
         }
         
-        const textEngIntro = COMMENTS.eng.intro.sampleText;
+        const textEngIntro = COMMENTS.eng.intro.grid;
         for(let r = 0; r < textEngIntro[keysIntro[0]].col1.length; r++) {
             textEngIntro[keysIntro[0]].col1[r].gridId = gridIdsIntro[0];
             textEngIntro[keysIntro[0]].col1[r].row = r + 1;
             textEngIntro[keysIntro[0]].col1[r].colId = "col1";
         }
-        function f (data, arr) {
+        function f1 (data, arr) {
             const _data = JSON.parse(JSON.stringify(data));
             arr.forEach(obj => {
                 _data.forEach((d, i) => {
                     Object.keys(d).forEach(k => {
                         if(k === obj.key) {
                             d[k] = obj.value[i].text;
+                            console.log(d[k]);
+                        }
+                    });
+                });
+            });
+            return _data;
+        }
+        function f2 (data, arr) {
+            const _data = JSON.parse(JSON.stringify(data));
+            arr.forEach(obj => {
+                _data.forEach((d, i) => {
+                    Object.keys(d).forEach(k => {
+                        if(k === obj.key) {
+                            console.log(obj);
+                            if(obj.value) {
+                                d[k] = {
+                                    value: obj.value[i].value,
+                                    text: obj.value[i].text,
+                                    target : "_self",
+                                };
+                            }
                         }
                     });
                 });
@@ -94,35 +115,75 @@ function getSampleDatas(COMMENTS) {
             [keysIntro[1]] :
             [
                 {
-                    col1 : {value:"https://vanilla-grid.github.io/",text:"id", target:"_self"},
+                    col1 : {value: null, text: null, target:"_self"},
                     col2 : {value:"https://vanilla-grid.github.io/",text:"id", target:"_self"},
                     col3 : {value:"https://vanilla-grid.github.io/",text:"onActiveCell", target:"_self"},
                     col4 : {value:"https://vanilla-grid.github.io/",text:"getHeaderRowCount", target:"_self"},
+                },
+                {
+                    col1 : {value: null, text: null, target:"_self"},
+                    col2 : {value:"https://vanilla-grid.github.io/",text:"name", target:"_self"},
+                    col3 : {value:"https://vanilla-grid.github.io/",text:"onActiveRow", target:"_self"},
+                    col4 : {value:"https://vanilla-grid.github.io/",text:"getHeaderText", target:"_self"},
+                },
+                {
+                    col1 : {value: null, text: null, target:"_self"},
+                    col2 : {value:"https://vanilla-grid.github.io/",text:"header", target:"_self"},
+                    col3 : {value:"https://vanilla-grid.github.io/",text:"onActiveCol", target:"_self"},
+                    col4 : {value:"https://vanilla-grid.github.io/",text:"setHeaderText", target:"_self"},
+                },
+                {
+                    col1 : {value: null, text: null, target:"_self"},
+                    col2 : {value:"https://vanilla-grid.github.io/",text:"footer", target:"_self"},
+                    col3 : {value:"https://vanilla-grid.github.io/",text:"onActiveCells", target:"_self"},
+                    col4 : {value:"https://vanilla-grid.github.io/",text:"reloadFilterValue", target:"_self"},
+                },
+                {
+                    col1 : {value: null, text: null, target:"_self"},
+                    col2 : {value:"https://vanilla-grid.github.io/",text:"dataType", target:"_self"},
+                    col3 : {value:"https://vanilla-grid.github.io/",text:"onActiveRows", target:"_self"},
+                    col4 : {value:"https://vanilla-grid.github.io/",text:"reloadColFilter", target:"_self"},
                 },
             ],
         }
 
         this.kor = {
             intro : {
-                [keysIntro[0]] : f(
+                [keysIntro[0]] : f1(
                     intro[[keysIntro[0]]],
                     [
-                        {key : "col1", value : textKorIntro[keysIntro[0]].col1}
+                        {key : "col1", value : textKorIntro[keysIntro[0]].col1},
                     ]
                 ),
-                [keysIntro[1]] : intro[[keysIntro[1]]],
+                [keysIntro[1]] : f2(
+                    intro[[keysIntro[1]]],
+                    [
+                        {key : "col1", value : textKorIntro[keysIntro[1]].col1},
+                        {key : "col2", value : textKorIntro[keysIntro[1]].col2},
+                        {key : "col3", value : textKorIntro[keysIntro[1]].col3},
+                        {key : "col4", value : textKorIntro[keysIntro[1]].col4},
+                    ]
+                ),
             },
         };
 
         this.eng = {
             intro : {
-                [keysIntro[0]] : f(
+                [keysIntro[0]] : f1(
                     intro[[keysIntro[0]]],
                     [
-                        {key : "col1", value : textEngIntro[keysIntro[0]].col1}
+                        {key : "col1", value : textEngIntro[keysIntro[0]].col1},
                     ]
                 ),
-                [keysIntro[1]] : intro[[keysIntro[1]]],
+                [keysIntro[1]] : f2(
+                    intro[[keysIntro[1]]],
+                    [
+                        {key : "col1", value : textEngIntro[keysIntro[1]].col1},
+                        {key : "col2", value : textEngIntro[keysIntro[1]].col2},
+                        {key : "col3", value : textEngIntro[keysIntro[1]].col3},
+                        {key : "col4", value : textEngIntro[keysIntro[1]].col4},
+                    ]
+                ),
             }
         };
     }
