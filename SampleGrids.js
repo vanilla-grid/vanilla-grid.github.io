e649891675515a5900e03190825a950fa0552ae2
vanilla-grid <vanilla.grid.co@gmail.com>
2024-06-17 14:32:06 +0900

    Update SampleGrids.js

--- SampleGrids.js
+++ SampleGrids.js
@@ -19,15 +19,13 @@ function getSampleGrids(COMMENTS) {
     <v-col id="col9" header=";column9;link" dataType="link" width="150px"></v-col>
 </vanilla-grid>`,
             [keysIntro[1]] :
-`<vanilla-grid id="` + gridIdsIntro[1] + `" rownumVisible="false" statusVisible="false">
+`<vanilla-grid id="` + gridIdsIntro[1] + `" rownumVisible="false" statusVisible="false" wordBreak="break-all" whiteSpace="normal">
     <v-col id="col1" header="grid\nattributes" dataType="link" width="25%"></v-col>
     <v-col id="col2" header="column\nattributes" dataType="link" width="25%"></v-col>
     <v-col id="col3" header="events" dataType="link" width="25%"></v-col>
     <v-col id="col4" header="methods" dataType="link" width="25%"></v-col>
 </vanilla-grid>`,
             }
-
-        
     }
     return new SampleGrids(COMMENTS);
 };
