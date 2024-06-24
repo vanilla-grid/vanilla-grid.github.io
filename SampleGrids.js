4d9ada60402be56247590a0e1c6aca330235c158
hisondev <64723261+hisondev@users.noreply.github.com>
2024-06-24 16:38:06 +0900

    Update SampleGrids.js

--- SampleGrids.js
+++ SampleGrids.js
@@ -1,13 +1,8 @@
-function getSampleGrids(COMMENTS) {
-    function SampleGrids (COMMENTS) {
-        const keysIntro = COMMENTS.SAMPLE_KEYS_INTRO;
-        const gridIdsIntro = [];
-        for(let i = 0; i < keysIntro.length; i++) {
-            gridIdsIntro.push("grid" + keysIntro[i])
-        }
+function getSampleGrids(CONSTS) {
+    function SampleGrids (CONSTS) {
         this.intro = {
-            [keysIntro[0]] :
-`<vanilla-grid id="` + gridIdsIntro[0] + `">
+            [CONSTS.SAMPLE_KEYS_INTRO[0]] :
+`<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_INTRO[0] + `" checkedValue="T" uncheckedValue="F">
     <v-col id="col1" header="header;column1;text" dataType="text" width="270px"></v-col>
     <v-col id="col2" header=";column2;code" dataType="code" codes="A;B;C;D;E" width="80px"></v-col>
     <v-col id="col3" header=";column3;mask" dataType="mask" format="AAAA-999" width="120px"></v-col>
@@ -18,14 +13,20 @@ function getSampleGrids(COMMENTS) {
     <v-col id="col8" header=";column8;button" dataType="button" width="120px"></v-col>
     <v-col id="col9" header=";column9;link" dataType="link" width="150px"></v-col>
 </vanilla-grid>`,
-            [keysIntro[1]] :
-`<vanilla-grid id="` + gridIdsIntro[1] + `" rownumVisible="false" statusVisible="false" wordBreak="break-all" whiteSpace="normal">
+            [CONSTS.SAMPLE_KEYS_INTRO[1]] :
+`<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_INTRO[1] + `" rownumVisible="false" statusVisible="false" wordBreak="break-all" whiteSpace="normal">
     <v-col id="col1" header="grid\nattributes" dataType="link" width="25%"></v-col>
     <v-col id="col2" header="column\nattributes" dataType="link" width="25%"></v-col>
     <v-col id="col3" header="events" dataType="link" width="25%"></v-col>
     <v-col id="col4" header="methods" dataType="link" width="25%"></v-col>
 </vanilla-grid>`,
-            }
+        };
+        this.started = {
+            [CONSTS.SAMPLE_KEYS_STARTED[0]] :
+`<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_STARTED[0] + `">
+    <v-col id="col1" header="text" dataType="text" width="400px"></v-col>
+</vanilla-grid>`,
+        }
     }
-    return new SampleGrids(COMMENTS);
+    return new SampleGrids(CONSTS);
 };
