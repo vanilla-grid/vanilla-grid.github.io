12959150c02344c40bc603aaf8be2745a7a0f248
vanilla-grid <vanilla.grid.co@gmail.com>
2024-06-25 17:00:24 +0900

    Update SampleGrids.js

--- SampleGrids.js
+++ SampleGrids.js
@@ -6,12 +6,13 @@ function getSampleGrids(CONSTS) {
     <v-col id="col1" header="header;column1;text" dataType="text" width="270px"></v-col>
     <v-col id="col2" header=";column2;code" dataType="code" codes="A;B;C;D;E" width="80px"></v-col>
     <v-col id="col3" header=";column3;mask" dataType="mask" format="AAAA-999" width="120px"></v-col>
-    <v-col id="col4" header=";column4;date" dataType="date" footer="MAX;MIN;SUM;AVG" width="120px"></v-col>
-    <v-col id="col5" header=";column5;number" dataType="number" footer="$$MAX;$$MIN;$$SUM;$$AVG" format="$ #,##0.####" width="120px"></v-col>
-    <v-col id="col6" header=";column6;select" dataType="select" width="120px"></v-col>
-    <v-col id="col7" header=";column7;checkbox" dataType="checkbox" width="100px"></v-col>
-    <v-col id="col8" header=";column8;button" dataType="button" width="120px"></v-col>
-    <v-col id="col9" header=";column9;link" dataType="link" width="150px"></v-col>
+    <v-col id="col4" header=";column4;date" dataType="date" width="120px"></v-col>
+    <v-col id="col5" header=";column4;month" dataType="month" footer="MAX;MIN;SUM;AVG" width="120px"></v-col>
+    <v-col id="col6" header=";column5;number" dataType="number" footer="$$MAX;$$MIN;$$SUM;$$AVG" format="$ #,##0.####" width="120px"></v-col>
+    <v-col id="col7" header=";column6;select" dataType="select" width="120px"></v-col>
+    <v-col id="col8" header=";column7;checkbox" dataType="checkbox" width="100px"></v-col>
+    <v-col id="col9" header=";column8;button" dataType="button" width="120px"></v-col>
+    <v-col id="col10" header=";column9;link" dataType="link" width="150px"></v-col>
 </vanilla-grid>`,
             [CONSTS.SAMPLE_KEYS_INTRO[1]] :
 `<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_INTRO[1] + `" rownumVisible="false" statusVisible="false" wordBreak="break-all" whiteSpace="normal">
@@ -24,7 +25,22 @@ function getSampleGrids(CONSTS) {
         this.started = {
             [CONSTS.SAMPLE_KEYS_STARTED[0]] :
 `<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_STARTED[0] + `">
-    <v-col id="col1" header="text" dataType="text" width="400px"></v-col>
+    <v-col id="col1" header="row1;row2;row3" dataType="text" width="400"></v-col>
+</vanilla-grid>`,
+            [CONSTS.SAMPLE_KEYS_STARTED[1]] :
+`<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_STARTED[1] + `">
+    <v-col id="dept" header="Click the Sigma button to the left of the header;filter1(mask);dept " dataType="mask" format="AAA99" width="100"></v-col>
+    <v-col id="f_nm" header=";filter2(text);first name" dataType="text" width="120" align="center"></v-col>
+    <v-col id="l_nm" header=";filter3(text);last name" dataType="text" width="120" align="center"></v-col>
+    <v-col id="d_o_j" header=";filter4(month);DOJ" dataType="month" width="120"></v-col>
+    <v-col id="e_id" header="Please double click;sort1(text);Employee ID" dataType="text" width="120" align="center"></v-col>
+    <v-col id="salary" header=";sort2(number);salary" dataType="number" format="$ #,###.00" width="150"></v-col>
+    <v-col id="status" header=";checkbox;status" dataType="checkbox" width="150"></v-col>
+</vanilla-grid>`,
+            [CONSTS.SAMPLE_KEYS_STARTED[2]] :
+`<vanilla-grid id="` + CONSTS.SAMPLE_GRID_IDS_STARTED[2] + `" nullValue="-">
+    <v-col id="col1" header="footer\ntitle" dataType="text" width="110" footer="Maximum :;Minimum :;Summation :;Average :" align="center"></v-col>
+    <v-col id="col2" header="footer\ntest" dataType="number" width="200" footer="$$MAX;$$MIN;$$SUM;$$AVG"></v-col>
 </vanilla-grid>`,
         }
     }
